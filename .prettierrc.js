c8e56410506362a015fcad903efb6e46246b0cdd
PitzTech <53540002+PitzTech@users.noreply.github.com>
2021-08-22 11:55:15 -0300

    Update Setup Configuration

---
+++ .prettierrc.js
@@ -0,0 +1,11 @@
+module.exports = {
+	useTabs: true,
+	tabWidth: 3,
+	bracketSpacing: true,
+	singleQuote: false,
+	semi: false,
+	arrowParens: "avoid",
+	trailingComma: "none",
+	proseWrap: "preserve",
+	jsxBracketSameLine: false
+}
---
+++ src/server.ts
@@ -0,0 +1,10 @@
+import "reflect-metadata";
+import express from "express";
+
+import "../db";
+
+const app = express();
+
+app.listen(3000, () => {
+	console.log("🚀 Server started on port 3000!");
+});
