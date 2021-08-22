6c47737a5f7795a19518d612556e64e056da4f69
PitzTech <53540002+PitzTech@users.noreply.github.com>
2021-08-22 16:57:55 -0300

    add error handler

--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,27 @@
 import "reflect-metadata"
-import express from "express"
-
-import "../db"
+import express, { Request, Response, NextFunction } from "express"
+import "express-async-errors"
 
 import { router } from "./routes"
+
+import "../db"
 
 const app = express()
 
 app.use(express.json())
 app.use(router)
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+	if (err instanceof Error) {
+		return res.status(400).json({
+			error: err.message,
+		})
+	}
+
+	return res.status(500).json({
+		status: "error",
+		message: "Internal Server Error",
+	})
+})
 
 app.listen(3000, () => {
 	console.log("🚀 Server started on port 3000!")
