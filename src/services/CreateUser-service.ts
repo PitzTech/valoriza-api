c53635ef8d0384765f26bf52bd42ae532c850326
PitzTech <53540002+PitzTech@users.noreply.github.com>
2021-08-26 08:48:17 -0300

    Fix Admin Info

--- src/services/CreateUser-service.ts
+++ src/services/CreateUser-service.ts
@@ -10,7 +10,7 @@ interface IUserRequest {
 }
 
 class CreateUserService {
-	async execute({ name, email, admin, password }: IUserRequest) {
+	async execute({ name, email, admin = false, password }: IUserRequest) {
 		const usersRepository = getCustomRepository(UserRepositories)
 
 		if (!name) throw new Error("Name is missing")
